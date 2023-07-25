85320b15cd80e2b79f3073b1056790d78d914341
Gaurav <23628920+gaurav-js-dev@users.noreply.github.com>
2023-07-25 17:04:46 +0530

    Return mocked component

--- apps/star-wars/src/app/app.spec.tsx
+++ apps/star-wars/src/app/app.spec.tsx
@@ -2,17 +2,26 @@ import { act, fireEvent, render, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
+import { Films } from './films';
 
 // jest.mock('film/module', () => ({ Films: jest.fn() }), { virtual: true });
 
-
-jest.mock('./films', () => ({ Films: jest.fn() }));
+// jest.mock('./films', () => ({ Films: jest.fn() }));
 // eslint-disable-next-line import/first
-import { Films } from './films';
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-(Films as any).mockImplementation(() => <div>Mock</div>);
+// (Films as any).mockImplementation(() => <div>Mock</div>);
 
+// jest.mock('films/Module', () => ({
+//   Film: () => {
+//     return <Films />;
+//   },
+// }));
+
+jest.mock('films/Module', () => ({
+  Film: () => {
+    return <Films />;
+  },
+}));
 
 it('should render successfully', () => {
   const { baseElement } = render(
